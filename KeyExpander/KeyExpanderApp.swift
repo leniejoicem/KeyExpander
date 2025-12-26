105e9e2e09933a297af35eb5d4724dcb74918741
leniejoicem <mendozaljay@gmail.com>
2025-12-26 22:58:14 +0800

    Initial Commit

---
+++ KeyExpander/KeyExpanderApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeyExpanderApp.swift
+//  KeyExpander
+//
+//  Created by Lenie Joice on 12/26/25.
+//
+
+import SwiftUI
+
+@main
+struct KeyExpanderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
